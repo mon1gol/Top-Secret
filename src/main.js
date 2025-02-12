395e95554ab5de147403ee087390bd0964ab02e3
mon1gol <profile4634@yandex.ru>
2025-02-13 00:09:50 +0300

    tournament cards and api request

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import './assets/main.css'
-
+import axios from 'axios'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 
 
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+app.use(router, axios)
 
 app.mount('#app')
