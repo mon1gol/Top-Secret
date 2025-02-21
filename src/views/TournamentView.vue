c0c9e33f0b443698dda543d0878ff859ee25e805
mon1gol <profile4634@yandex.ru>
2025-02-21 11:29:43 +0300

    can view the card

---
+++ src/views/TournamentView.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="bg-white-one w-lvw p-20 grid grid-cols-2 gap-15">
+    <div>
+      <h1 class="text-6xl">{{ tournament.name }}</h1>
+      <div class="mt-10">{{ tournament.description }}</div>
+      <div class="mt-10 flex flex-col gap-2">
+        <div>Дата начала: </div>
+        <div>Дата окончания: </div>
+        <div>В команде должно быть от до участников</div>
+      </div>
+      <button class="bg-blue-four mt-10 px-10 py-5 cursor-pointer rounded-2xl">Подать заявку</button>
+    </div>
+    <div class="">
+      <img class="h-100 w-150 shadow-2xl" :src="tournament.get_image" :alt="tournament.name">
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'TournamentView',
+  data() {
+    return {
+      tournament: {}
+    }
+  },
+  mounted(){
+    this.getTournament()
+  },
+  methods: {
+    getTournament(){
+      const category_slug = this.$route.params.category_slug
+      const tournament_slug = this.$route.params.tournament_slug
+
+      axios
+        .get(`/api/v1/tournaments/${category_slug}/${tournament_slug}/`)
+        .then(Response =>{
+          this.tournament = Response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
