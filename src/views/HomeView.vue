093e92451b37ce3dccca18abe4451ce936d11c3f
mon1gol <profile4634@yandex.ru>
2025-02-24 22:47:28 +0300

    categories are working

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,17 +3,25 @@
     <div class="p-10 pt-40">
       <div class="text-6xl font-bold text-center">Соревнования</div>
 
-      <div class="mt-30 bg-blue-one p-5 w-full flex flex-wrap justify-center gap-x-20 gap-y-5 relative rounded-xl shadow-sm">
-        <div class="px-10 h-full cursor-pointer category-btn-hover-dark relative z-10">Все</div>
-        <div class="px-10 h-full cursor-pointer category-btn-hover-dark relative z-10">Хакатоны</div>
-        <div class="px-10 h-full cursor-pointer category-btn-hover-dark relative z-10">Олимпиады</div>
+      <div
+        class="mt-30 bg-blue-one p-5 w-full flex flex-wrap justify-center gap-x-20 gap-y-5 relative rounded-xl shadow-sm">
+        <div class="px-10 h-full cursor-pointer category-btn-hover-dark relative z-10"
+          @click="getCategoryBySlug('all')">
+          Все
+        </div>
+        <div class="px-10 h-full cursor-pointer category-btn-hover-dark relative z-10"
+          @click="getCategoryBySlug('hackathons')">
+          Хакатоны
+        </div>
+        <div class="px-10 h-full cursor-pointer category-btn-hover-dark relative z-10"
+          @click="getCategoryBySlug('olympiads')">
+          Олимпиады
+        </div>
       </div>
 
       <div class="mt-10 grid grid-cols-3 justify-items-center gap-y-10">
         <router-link :to="tournament.get_absolute_url" class="bg-blue-500 size-100 shadow-sm"
-          v-for="tournament in latestTournaments"
-          v-bind:key="tournament.id"
-        >
+          v-for="tournament in latestTournaments" :key="tournament.id">
           <img class="bg-blue-two h-70 w-100 object-cover" :src="tournament.get_image">
           <div class="p-5 text-2xl">{{ tournament.name }}</div>
         </router-link>
@@ -25,31 +33,48 @@
 <script>
 import axios from 'axios';
 
-export default{
+export default {
   name: 'HomeView',
   data() {
-    return{
+    return {
       latestTournaments: [],
-    }
+      activeCategory: 'all',
+    };
   },
-  components: {
-
-  },
-  mounted(){
-    this.getLatestTournaments()
-    document.title = 'Home'
+  mounted() {
+    this.getLatestTournaments();
+    document.title = 'Home';
   },
   methods: {
-    getLatestTournaments(){
+    getLatestTournaments() {
       axios
         .get('/api/v1/latest-tournaments/')
-        .then(Response =>{
-          this.latestTournaments = Response.data
+        .then((response) => {
+          this.latestTournaments = response.data;
         })
-        .catch(error => {
-          console.log(error)
-        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-  }
-}
+
+    getCategoryBySlug(slug) {
+      this.activeCategory = slug;
+      if (slug == 'all') {
+        this.getLatestTournaments()
+      }
+      else {
+        axios
+          .get(`/api/v1/tournaments/${slug}/`)
+          .then((response) => {
+            this.latestTournaments = response.data.tournaments;
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    },
+  },
+};
 </script>
+
+<style></style>
