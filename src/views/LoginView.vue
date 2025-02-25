bb51d2936778be7fe0396c51c9a581ddd726ad34
mon1gol <profile4634@yandex.ru>
2025-02-25 17:11:50 +0300

    added signup and login pages

---
+++ src/views/LoginView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="gradient-blue flex flex-col h-lvh w-lvw items-center justify-center">
+    <h1 class="mt-20 mb-10 text-5xl font-medium">Вход</h1>
+    <div class="h-120 w-100 px-10 py-15 rounded-xl">
+      <form @submit.prevent="submitForm" class="flex flex-col gap-5">
+        <div>
+          <label for="username" class="block text-sm/6 font-semibold text-black">Логин</label>
+          <div class="mt-2">
+            <input v-model="username" placeholder="Введите" type="text" class="block w-full rounded-xl bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 placeholder:text-sm focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 transition-all duration-150">
+          </div>
+        </div>
+        <div>
+          <label for="password" class="block text-sm/6 font-semibold text-black">Пароль</label>
+          <div class="mt-2">
+            <input v-model="password" placeholder="Введите" type="password" class="block w-full rounded-xl bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 placeholder:text-sm focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 transition-all duration-150">
+          </div>
+        </div>
+        <button class="mt-7 block w-full rounded-xl bg-blue-three btn-hover-dark z-3 px-3.5 py-2 text-white cursor-pointer">Регистрация</button>
+        <!-- <div class="text-red-500 text-center" v-if="errors.length">
+          <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
+        </div> -->
+        <hr>
+          <div class="text-center">Или <router-link class="text-blue-700 border-b-1 hover:text-blue-900" to="/sign-up">зарегестрируйтесь</router-link></div>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "LoginView",
+}
+</script>
+<style>
+
+</style>
---
+++ src/views/SignUpView.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="gradient-blue flex flex-col h-lvh w-lvw items-center justify-center">
+    <h1 class="mt-20 mb-10 text-5xl font-medium">Регистрация</h1>
+    <div class="h-120 w-100 px-10 py-15 rounded-xl">
+      <form @submit.prevent="submitForm" class="flex flex-col gap-5">
+        <div>
+          <label for="username" class="block text-sm/6 font-semibold text-black">Логин</label>
+          <div class="mt-2">
+            <input v-model="username" placeholder="Введите" type="text" class="block w-full rounded-xl bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 placeholder:text-sm focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 transition-all duration-150">
+          </div>
+        </div>
+        <div>
+          <label for="password" class="block text-sm/6 font-semibold text-black">Пароль</label>
+          <div class="mt-2">
+            <input v-model="password" placeholder="Введите" type="password" class="block w-full rounded-xl bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 placeholder:text-sm focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 transition-all duration-150">
+          </div>
+        </div>
+        <div>
+          <label for="password2" class="block text-sm/6 font-semibold text-black">Повторите пароль</label>
+          <div class="mt-2">
+            <input v-model="password2" placeholder="Введите" type="password" class="block w-full rounded-xl bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 placeholder:text-sm focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 transition-all duration-150">
+          </div>
+        </div>
+        <button class="mt-7 block w-full rounded-xl bg-blue-three btn-hover-dark z-3 px-3.5 py-2 text-white cursor-pointer">Регистрация</button>
+        <div class="text-red-500 text-center" v-if="errors.length">
+          <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
+        </div>
+        <hr>
+          <div class="text-center">Или <router-link class="text-blue-700 border-b-1 hover:text-blue-900" to="/log-in">войдите</router-link> в аккаунт</div>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+import axios from 'axios';
+
+export default {
+  name: "SignUpView",
+  data(){
+    return{
+      username: '',
+      password: '',
+      password2: '',
+      errors: []
+    }
+  },
+  mounted(){
+
+  },
+  methods:{
+    submitForm(){
+      this.errors = []
+
+      if(this.username === '' || this.password === ''){
+        this.errors.push('Заполните все поля')
+      }
+
+      if(this.password !== this.password2){
+        this.errors.push('Пароли не совпадают')
+      }
+
+      if(!this.errors.length){
+        const formData = {
+          username: this.username,
+          password: this.password
+        }
+        axios
+          .post("/api/v1/users/", formData)
+          .then(Response => {
+            this.$router.push('/log-in')
+          })
+          .catch(error => {
+            if(error.Response){
+              for(const property in error.Response.data){
+                this.errors.push(`${property}: ${error.Response.data[property]}`)
+              }
+
+              console.log(JSON.stringify(error.Response.data))
+            }
+            else if(error.message) {
+              this.errors.push('Произошла ошибка, попробуйте позже')
+
+              console.log(JSON.stringify(error))
+            }
+          })
+      }
+    }
+  }
+}
+</script>
+<style>
+
+</style>
