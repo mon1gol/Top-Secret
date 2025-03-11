be56e7c9279480bcd8451664efe41d033c2cb6af
mon1gol <profile4634@yandex.ru>
2025-03-11 15:15:38 +0300

    fix route to status

--- main/urls.py
+++ main/urls.py
@@ -6,8 +6,8 @@ from main import views
 urlpatterns = [
     path('users-list/', views.UsersList.as_view()),
     path('latest-tournaments/', views.LatestTournamentsList.as_view()),
-    path('tournaments/<slug:status_slug>/', views.TournamentsByStatus.as_view()),
     path('tournaments/<slug:category_slug>/', views.CategoryDetail.as_view()),
+    path('tournaments/status/<slug:status_slug>/', views.TournamentsByStatus.as_view()),
     path('tournaments/<slug:category_slug>/<slug:tournament_slug>/', views.TournamentDetail.as_view()),
     path('tournaments/<slug:category_slug>/<slug:tournament_slug>/teams/', views.TeamActions.as_view()),
 ]
