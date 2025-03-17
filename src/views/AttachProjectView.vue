b330d2ecb7fe9534a5a48eb5464b5b095f1a2d88
mon1gol <profile4634@yandex.ru>
2025-03-17 18:24:32 +0300

    attaching project to tournament is working

--- src/views/AttachProjectView.vue
+++ src/views/AttachProjectView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="p-10 pt-40">
     <div class="text-6xl font-bold text-center">Прикрепить проект</div>
-    <h2 class="mt-10 text-3xl text-center">Выберите соревнования</h2>
+    <h2 class="mt-10 text-3xl text-center">Выберите соревнование</h2>
 
     <div v-if="!tournaments.length" class="flex justify-center mt-40 flex-col items-center gap-10">
       <h1 class="text-2xl text-gray-500">На данный момент у вас нет активных мероприятий</h1>
@@ -11,7 +11,7 @@
     </div>
 
     <div class="mt-40 grid grid-cols-3 justify-items-center gap-y-10">
-      <router-link :to="tournament.get_absolute_url" class="bg-blue-200 size-100 shadow-sm overflow-hidden rounded-xl"
+      <router-link :to="'/attach-project' + tournament.get_absolute_url" class="bg-blue-200 size-100 shadow-sm overflow-hidden rounded-xl"
         v-for="tournament in tournaments" :key="tournament.id">
         <img class="bg-blue-two h-70 w-100 object-cover" :src="tournament.get_image">
         <div class="p-5 text-2xl">{{ tournament.name }}</div>
