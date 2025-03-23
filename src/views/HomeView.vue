e107bff2b55a10dbbd5084b84aa81b5d2b71adfe
mon1gol <profile4634@yandex.ru>
2025-03-23 19:53:15 +0300

    created component cardsTournament

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,41 +1,37 @@
 <template>
   <main class="bg-white-one w-lvw">
-    <div class="p-10 pt-40">
-      <div class="text-6xl font-bold text-center">Соревнования</div>
+    <div class="pt-40 text-6xl font-bold text-center">Соревнования</div>
 
-      <div
-        class="mt-30 bg-blue-one p-5 w-full flex flex-wrap justify-center gap-x-20 gap-y-5 relative rounded-xl shadow-sm">
-        <div class="px-10 h-full cursor-pointer category-btn-hover-dark relative z-5"
-          @click="getCategoryBySlug('all')">
-          Все
-        </div>
-        <div class="px-10 h-full cursor-pointer category-btn-hover-dark relative z-5"
-          @click="getCategoryBySlug('hackathons')">
-          Хакатоны
-        </div>
-        <div class="px-10 h-full cursor-pointer category-btn-hover-dark relative z-5"
-          @click="getCategoryBySlug('olympiads')">
-          Олимпиады
-        </div>
+    <div
+      class="mt-30 bg-blue-one p-5 w-full flex flex-wrap justify-center gap-x-20 gap-y-5 relative rounded-xl shadow-sm">
+      <div class="px-10 h-full cursor-pointer category-btn-hover-dark relative z-5"
+        @click="getCategoryBySlug('all')">
+        Все
       </div>
-
-      <div class="mt-10 grid grid-cols-3 justify-items-center gap-y-10">
-        <router-link :to="tournament.get_absolute_url" class="bg-blue-200 size-100 shadow-sm overflow-hidden rounded-xl"
-          v-for="tournament in latestTournaments" :key="tournament.id">
-          <img class="bg-blue-two h-70 w-100 object-cover" :src="tournament.get_image">
-          <div class="p-5 text-2xl">{{ tournament.name }}</div>
-        </router-link>
+      <div class="px-10 h-full cursor-pointer category-btn-hover-dark relative z-5"
+        @click="getCategoryBySlug('hackathons')">
+        Хакатоны
+      </div>
+      <div class="px-10 h-full cursor-pointer category-btn-hover-dark relative z-5"
+        @click="getCategoryBySlug('olympiads')">
+        Олимпиады
       </div>
     </div>
+
+    <CardsTournamentComponent :tournaments="this.latestTournaments" :linkIncrement="''" :isHome="true"/>
   </main>
 </template>
 
 <script>
+import CardsTournamentComponent from '@/components/CardsTournamentComponent.vue';
 import axios from 'axios';
 
 
 export default {
   name: 'HomeView',
+  components:{
+    CardsTournamentComponent,
+  },
   data() {
     return {
       latestTournaments: [],
--- src/views/MyTournamentsView.vue
+++ src/views/MyTournamentsView.vue
@@ -16,26 +16,19 @@
         Завершенные
       </div>
     </div>
-
-    <div v-if="!tournaments.length" class="flex justify-center mt-30">
-      <h1 class="text-2xl text-gray-500">На данный момент мероприятий нет</h1>
-    </div>
-
-    <div v-else class="mt-10 grid grid-cols-3 justify-items-center gap-y-10">
-      <router-link :to="tournament.get_absolute_url" class="bg-blue-200 size-100 shadow-sm overflow-hidden rounded-xl"
-        v-for="tournament in tournaments" :key="tournament.id">
-        <img class="bg-blue-two h-70 w-100 object-cover" :src="tournament.get_image">
-        <div class="p-5 text-2xl">{{ tournament.name }}</div>
-      </router-link>
-    </div>
+    <CardsTournamentComponent :tournaments="this.tournaments" :linkIncrement="''" :isHome="false"/>
   </div>
 </template>
 <script>
+import CardsTournamentComponent from '@/components/CardsTournamentComponent.vue';
 import axios from 'axios';
 
 
 export default {
   name: "MyTournamentsView",
+  components:{
+    CardsTournamentComponent,
+  },
   data() {
     return {
       tournaments:[],
