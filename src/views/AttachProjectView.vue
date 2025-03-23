02b6fa9f948073c3ae5b1ef68e4225b7d73c931a
mon1gol <profile4634@yandex.ru>
2025-03-23 19:53:15 +0300

    created component cardsTournament

--- src/views/AttachProjectView.vue
+++ src/views/AttachProjectView.vue
@@ -2,30 +2,21 @@
   <div class="p-10 pt-40">
     <div class="text-6xl font-bold text-center">Прикрепить проект</div>
     <h2 class="mt-10 text-3xl text-center">Выберите соревнование</h2>
-
-    <div v-if="!tournaments.length" class="flex justify-center mt-40 flex-col items-center gap-10">
-      <h1 class="text-2xl text-gray-500">На данный момент у вас нет активных мероприятий</h1>
-      <router-link to="/">
-        <button class="text-white bg-blue-four hover:opacity-90 cursor-pointer rounded-2xl px-10 py-5">К соренованиям</button>
-      </router-link>
-    </div>
-
-    <div class="mt-40 grid grid-cols-3 justify-items-center gap-y-10">
-      <router-link :to="'/attach-project' + tournament.get_absolute_url" class="bg-blue-200 size-100 shadow-sm overflow-hidden rounded-xl"
-        v-for="tournament in tournaments" :key="tournament.id">
-        <img class="bg-blue-two h-70 w-100 object-cover" :src="tournament.get_image">
-        <div class="p-5 text-2xl">{{ tournament.name }}</div>
-      </router-link>
-    </div>
+    <CardsTournamentComponent :tournaments="this.tournaments" :linkIncrement="'/attach-project'" :isHome="false"/>
   </div>
+
 </template>
 
 <script>
+import CardsTournamentComponent from '@/components/CardsTournamentComponent.vue';
 import axios from 'axios';
 
 
 export default {
   name: 'AttachProjectView',
+  components:{
+    CardsTournamentComponent,
+  },
   data() {
     return {
       tournaments:[],
