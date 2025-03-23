53b401eb034398a32918a3cef9bba5eeb31c6bd7
mon1gol <profile4634@yandex.ru>
2025-03-23 18:46:54 +0300

    add info displayed in tournament

--- src/views/TournamentView.vue
+++ src/views/TournamentView.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="bg-white-one w-lvw p-20 grid grid-cols-2 gap-15">
+  <div class="h-lvh bg-white-one w-lvw p-20 grid grid-cols-2 gap-15">
     <div>
       <h1 class="text-6xl">{{ tournament.name }}</h1>
       <div class="mt-10">{{ tournament.description }}</div>
       <div class="mt-10 flex flex-col gap-2">
-        <div>Дата начала: </div>
-        <div>Дата окончания: </div>
-        <div>В команде должно быть от до участников</div>
+        <div>
+          <div class="flex items-center">Дата начала: <div class="ml-2 bg-blue-two rounded-2xl p-1">{{ tournament_rules.date_start }}</div></div>
+        </div>
+        <div>
+          <div class="flex items-center">Дата окончания: <div class="ml-2 bg-blue-two rounded-2xl p-1">{{ tournament_rules.date_end }}</div></div>
+        </div>
+        <div>В команде должно быть от {{ tournament_rules.min_members_in_team }} до {{ tournament_rules.max_members_in_team }} участников</div>
       </div>
       <router-link :to="tournament.get_absolute_url + 'submit'">
         <button class="bg-blue-four mt-10 cursor-pointer rounded-2xl px-10 py-5">Подать заявку</button>
@@ -25,7 +29,8 @@ export default {
   name: 'TournamentView',
   data() {
     return {
-      tournament: {}
+      tournament: {},
+      tournament_rules: {},
     }
   },
   mounted(){
@@ -40,6 +45,7 @@ export default {
         .get(`/api/v1/tournaments/${category_slug}/${tournament_slug}/`)
         .then(Response =>{
           this.tournament = Response.data
+          if(Response.data.rules){this.tournament_rules = Response.data.rules}
           document.title = this.tournament.name
         })
         .catch(error => {
