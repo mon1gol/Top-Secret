b1e8a2a6bbda3b427a428a5ce717dc77c38f37a8
mon1gol <profile4634@yandex.ru>
2025-03-29 17:30:58 +0300

    added team results list page

---
+++ src/views/MyResultsView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="px-50 pt-40">
+    <div class="text-6xl font-bold text-center mb-15">Результаты соревнований</div>
+      <div class="w-full space-y-10">
+        <div v-for="result in teams_result" :key="result.id">
+          <div v-if="result.assessments.length" class="bg-blue-one px-10 py-5 rounded-xl">
+            <h1 class="text-2xl">{{result.name}}</h1>
+            <h2>{{result.tournament}}</h2>
+            <div class="flex justify-end">
+              <router-link to="/" class="hover:underline">Доступны результаты ➡️</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+</template>
+<script>
+import axios from 'axios';
+
+
+export default {
+  name: 'MyResultsView',
+  data() {
+    return {
+      username: localStorage.getItem('username'),
+      teams_result: []
+    }
+  },
+  mounted() {
+    this.getTeamResults();
+  },
+  methods: {
+    getTeamResults() {
+      axios
+
+        .get(`/api/v1/team-results/${this.username}/`)
+        .then((response) => {
+          this.teams_result = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+}
+</script>
